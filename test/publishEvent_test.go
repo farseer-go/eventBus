509c2d2f892855d2882b2e6c504a1c041678296b
yangwei <123576128@qq.com>
2022-12-28 21:22:51 +0800

    解决 test 中的 race 问题

--- test/publishEvent_test.go
+++ test/publishEvent_test.go
@@ -4,14 +4,15 @@ import (
 	"github.com/farseer-go/eventBus"
 	"github.com/farseer-go/fs"
 	"github.com/stretchr/testify/assert"
+	"sync/atomic"
 	"testing"
 	"time"
 )
 
-var count int
+var count int32
 
 type testEventPublish struct {
-	count int
+	count int32
 }
 
 func TestPublishEvent(t *testing.T) {
@@ -19,19 +20,19 @@ func TestPublishEvent(t *testing.T) {
 
 	eventBus.Subscribe("test_event_subscribe", func(message any, ea eventBus.EventArgs) {
 		event := message.(testEventPublish)
-		count += event.count + 1
+		atomic.AddInt32(&count, event.count+1)
 	})
 
 	eventBus.Subscribe("test_event_subscribe", func(message any, ea eventBus.EventArgs) {
 		event := message.(testEventPublish)
-		count += event.count + 2
+		atomic.AddInt32(&count, event.count+2)
 	})
 
 	eventBus.PublishEvent("test_event_subscribe", testEventPublish{count: 6})
 	time.Sleep(10 * time.Millisecond)
-	assert.Equal(t, 15, count)
+	assert.Equal(t, int32(15), count)
 
 	eventBus.PublishEventAsync("test_event_subscribe", testEventPublish{count: 4})
 	time.Sleep(10 * time.Millisecond)
-	assert.Equal(t, 26, count)
+	assert.Equal(t, int32(26), count)
 }
