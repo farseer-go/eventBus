abc8113a9d05be4b58d5ecc23b836d3a5d34a3b8
steden <1470804@qq.com>
2022-12-31 21:35:57 +0800

    PublishEvent、PublishEventAsync 增加error返回

--- publishEvent.go
+++ publishEvent.go
@@ -10,11 +10,10 @@ import (
 )
 
 // PublishEvent 阻塞发布事件
-func PublishEvent(eventName string, message any) {
+func PublishEvent(eventName string, message any) error {
 	// 首先从订阅者中找到是否存在eventName
 	if !subscriber.ContainsKey(eventName) {
-		flog.Warningf("需要先通过订阅事件后，才能发布事件：%s", eventName)
-		return
+		return flog.Errorf("需要先通过订阅事件后，才能发布事件：%s", eventName)
 	}
 
 	// 定义事件参数
@@ -26,6 +25,7 @@ func PublishEvent(eventName string, message any) {
 	}
 
 	// 遍历订阅者，并同步执行事件消费
+	var err error
 	for _, subscribeFunc := range subscriber.GetValue(eventName) {
 		try := exception.Try(func() {
 			sw := stopwatch.StartNew()
@@ -33,16 +33,17 @@ func PublishEvent(eventName string, message any) {
 			flog.ComponentInfof("event", "%s，耗时：%s", eventName, sw.GetMillisecondsText())
 		})
 		try.CatchException(func(exp any) {
-			flog.Error(exp)
+			err = flog.Error(exp)
 		})
 	}
+	return err
 }
 
 // PublishEventAsync 异步发布事件
-func PublishEventAsync(eventName string, message any) {
+func PublishEventAsync(eventName string, message any) error {
 	// 首先从订阅者中找到是否存在eventName
 	if !subscriber.ContainsKey(eventName) {
-		panic("未找到事件名称：" + eventName + "，需要先通过订阅事件后，才能发布事件")
+		return flog.Errorf("需要先通过订阅事件后，才能发布事件：%s", eventName)
 	}
 
 	// 定义事件参数
@@ -60,8 +61,9 @@ func PublishEventAsync(eventName string, message any) {
 				subscribeFunc(message, eventArgs)
 			})
 			try.CatchException(func(exp any) {
-				flog.Error(exp)
+				_ = flog.Error(exp)
 			})
 		}(subscribeFunc)
 	}
+	return nil
 }
