cc9c3859a91089c6a466fc33d8b9ca1b0694c8d7
steden <1470804@qq.com>
2023-01-28 00:05:58 +0800

    添加事件通用接口

---
+++ registerEvent.go
@@ -0,0 +1,27 @@
+package eventBus
+
+import (
+	"github.com/farseer-go/fs/container"
+	"github.com/farseer-go/fs/core"
+)
+
+type registerEvent struct {
+	eventName string
+}
+
+func (c *registerEvent) Publish(message any) {
+	_ = PublishEvent("ClientOffline", message)
+}
+
+// RegisterEvent 注册core.IEvent实现
+func RegisterEvent(eventName string, fns ...consumerFunc) {
+	// 注册仓储
+	container.Register(func() core.IEvent {
+		return &registerEvent{eventName: eventName}
+	}, eventName)
+
+	// 同时订阅消费
+	for i := 0; i < len(fns); i++ {
+		Subscribe(eventName, fns[i])
+	}
+}
